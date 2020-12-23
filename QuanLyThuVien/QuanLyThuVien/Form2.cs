677885203f4d5fa1333f7ea249d69b2efabff7c8
Việt <70686146+VietBanna@users.noreply.github.com>
2020-12-23 09:20:43 +0700

    Update

--- QuanLyThuVien/QuanLyThuVien/Form2.cs
+++ QuanLyThuVien/QuanLyThuVien/Form2.cs
@@ -7,16 +7,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data;
 using System.Data.SqlClient;
+<<<<<<< HEAD
 using System.Configuration;
+=======
+
+>>>>>>> parent of 71d7b68... Như push QLTV
 namespace QuanLyThuVien
 {
     public partial class Form2 : Form
     {
+        private string link = @"DESKTOP-FV6UJS5\SQLEXPRESS;Initial Catalog=qltv;Integrated Security=True";
+        private SqlConnection connect;
+        private SqlCommand command;
         public Form2()
         {
             InitializeComponent();
+<<<<<<< HEAD
             panelHV.Visible = false;
                pn_TraSach.Visible = false;
                pn_muonsach.Visible = false;
@@ -46,12 +53,22 @@ namespace QuanLyThuVien
                pn_DauSach.Visible = false;
                panelHV.Dock = DockStyle.Fill;
             
+=======
+        }
+
+        private void btn_hocvien_Click(object sender, EventArgs e)
+        {
+            lb_control.Visible = true;
+            lb_control.Location = new Point(0, 2);
+            pn_sach.Visible = false;
+>>>>>>> parent of 71d7b68... Như push QLTV
         }
 
         private void btn_dausach_Click(object sender, EventArgs e) // button dau sach
         {
             lb_control.Visible = true;
             lb_control.Location = new Point(0, 69);
+<<<<<<< HEAD
                panelHV.Visible = false;
                pn_TraSach.Visible = false;
                pn_muonsach.Visible = false;
@@ -59,11 +76,16 @@ namespace QuanLyThuVien
                pn_DauSach.Visible = true;
                pn_DauSach.Dock = DockStyle.Fill;
           }
+=======
+            pn_sach.Visible = false;
+        }
+>>>>>>> parent of 71d7b68... Như push QLTV
 
         private void btn_sach_Click(object sender, EventArgs e) // button sach
         {
             lb_control.Visible = true;
             lb_control.Location = new Point(0, 136);
+<<<<<<< HEAD
                panelHV.Visible = false;
                pn_TraSach.Visible = false;
                pn_muonsach.Visible = false;
@@ -71,11 +93,16 @@ namespace QuanLyThuVien
                pn_DauSach.Visible = false;
                panelSACH.Dock = DockStyle.Fill;
           }
+=======
+            pn_sach.Visible = true;
+        }
+>>>>>>> parent of 71d7b68... Như push QLTV
 
         private void btn_muonsach_Click(object sender, EventArgs e) // muon sach
         {
             lb_control.Visible = true;
             lb_control.Location = new Point(0, 203);
+<<<<<<< HEAD
                panelHV.Visible = false;
                pn_TraSach.Visible = false;
                
@@ -85,12 +112,16 @@ namespace QuanLyThuVien
                pn_muonsach.Visible = true;
             pn_muonsach.Dock = DockStyle.Fill;
             panelHV.Visible = false;
+=======
+            pn_sach.Visible = false;
+>>>>>>> parent of 71d7b68... Như push QLTV
         }
 
         private void btn_trasach_Click(object sender, EventArgs e) // tra sach
         {
             lb_control.Visible = true;
             lb_control.Location = new Point(0, 270);
+<<<<<<< HEAD
                panelHV.Visible = false;
                
                pn_muonsach.Visible = false;
@@ -99,6 +130,9 @@ namespace QuanLyThuVien
                
                pn_TraSach.Visible = true;
             pn_TraSach.Dock = DockStyle.Fill;
+=======
+            pn_sach.Visible = false;
+>>>>>>> parent of 71d7b68... Như push QLTV
         }
 
         private void btn_exit_Click(object sender, EventArgs e) // thoat
@@ -106,6 +140,7 @@ namespace QuanLyThuVien
             this.Dispose();
         }
 
+<<<<<<< HEAD
         private void label8_Click(object sender, EventArgs e)
         {
 
@@ -1029,4 +1064,93 @@ namespace QuanLyThuVien
                }
           }
      }
+=======
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 7)
+            {
+                panel3.Visible = true;
+
+                textBox8.Text = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+                textBox12.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+                textBox11.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+                textBox7.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+                textBox10.Text = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+                textBox9.Text = dataGridView1.Rows[e.RowIndex].Cells[5].Value.ToString();
+                textBox1.Text = dataGridView1.Rows[e.RowIndex].Cells[6].Value.ToString();
+
+                //pn_sua_tkb.BringToFront();415, 393
+            }
+            else
+            {
+                if (e.ColumnIndex == 8)
+                {
+                    DialogResult result = MessageBox.Show("Bạn có chắc muốn xóa sách?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                    if (result == DialogResult.Yes)
+                    {
+                        using (connect = new SqlConnection(link))
+                        {
+                            connect.Open();
+                            command = new SqlCommand("delete from SACH where MASACH = '" + dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString() + "'", connect);
+                            command.ExecuteNonQuery();
+                        }
+                        Form2_Load(sender, e);
+                    }
+                }
+            }
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'qltvDataSet1.SACH' table. You can move, or remove it, as needed.
+            this.sACHTableAdapter.Fill(this.qltvDataSet1.SACH);
+
+        }
+
+        private void bt_them_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (connect = new SqlConnection(link))
+            {
+                connect.Open();
+                command = new SqlCommand("insert SACH values('" + textBox3.Text + "', N'" + textBox14.Text + "', '" + textBox13.Text + "', '" + textBox2.Text + "', '" + textBox6.Text + "','" + textBox5.Text + "','" + textBox4.Text + "')", connect);
+                command.ExecuteNonQuery();
+            }
+            panel1.Visible = false;
+            Form2_Load(sender, e);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            using (connect = new SqlConnection(link))
+            {
+                connect.Open();
+                command = new SqlCommand("update SACH set MASACH = N'" + textBox3.Text + "', TENSACH = '" + textBox14.Text + "', TRANGTHAI = '" + textBox13.Text + "', SOLUONG = '" + textBox2.Text + "',TENNXB = '" + textBox6.Text + "',TENTG = '" + textBox5.Text + "',MADAUSANH = '" + textBox4.Text + "'" +
+                    "where MASACH = '" + textBox3.Text + "'", connect);
+                command.ExecuteNonQuery();
+            }
+            panel3.Visible = false;
+            Form2_Load(sender, e);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            panel3.Visible = false;
+        }
+
+        private void btn_them_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+        }
+    }
+>>>>>>> parent of 71d7b68... Như push QLTV
 }
--- QuanLyThuVien/QuanLyThuVien/Properties/Settings.Designer.cs
+++ QuanLyThuVien/QuanLyThuVien/Properties/Settings.Designer.cs
@@ -12,7 +12,11 @@ namespace QuanLyThuVien.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+<<<<<<< HEAD
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+=======
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.6.0.0")]
+>>>>>>> parent of 71d7b68... Như push QLTV
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,10 +30,18 @@ namespace QuanLyThuVien.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+<<<<<<< HEAD
         [global::System.Configuration.DefaultSettingValueAttribute("Data Source=SCORPION;Initial Catalog=QuanlyThuvien;Integrated Security=True")]
         public string QuanlyThuvienConnectionString {
             get {
                 return ((string)(this["QuanlyThuvienConnectionString"]));
+=======
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-FV6UJS5\\SQLEXPRESS;Initial Catalog=qltv;Integrated Security=T" +
+            "rue")]
+        public string qltvConnectionString {
+            get {
+                return ((string)(this["qltvConnectionString"]));
+>>>>>>> parent of 71d7b68... Như push QLTV
             }
         }
     }
